cbfe5f102da895731fdaec00fc2b1bcde5cb5850
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-08-06 20:47:05 +0200

    refactor: simplified error structure

--- src/common/isKnownError.ts
+++ src/common/isKnownError.ts
@@ -1,7 +1,6 @@
-import { isApplicationError } from '@/application-layer/common/applicationErrors';
 import { isDomainError } from '@/domain/common/domainErrors';
 import { isAdapterError } from '@/interface-adapters/common/adapterErrors';
 
 export function isKnownError(error: Error): boolean {
-  return isDomainError(error) || isApplicationError(error) || isAdapterError(error);
+  return isDomainError(error) || isAdapterError(error);
 }
