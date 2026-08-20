3775033fcf1434870f56b74f2e00c67cc4a6b5ae
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-08-20 21:49:53 +0200

    feat: created MongooseUsersRepo

--- src/application-layer/dtos/__tests__/UserDTO.test.ts
+++ src/application-layer/dtos/__tests__/UserDTO.test.ts
@@ -4,7 +4,7 @@ import { createTestUser } from '@/../tests/createEntitiesTest/userCreate';
 import { userDTOProperties } from '@/../tests/dtoProperties/userDtoProperties';
 import { User } from '@/domain/entities/User/User';
 
-import { UserDTO, toUserDTO } from '../UserDTO';
+import { UserDTO, toUserDTO, toUserFromDTO } from '../UserDTO';
 
 describe('UserDTO', () => {
   let user: User;
@@ -39,6 +39,52 @@ describe('UserDTO', () => {
         createdAt: user.createdAt.toISOString(),
         updatedAt: user.updatedAt.toISOString(),
       });
+    });
+  });
+
+  describe('toUserFromDTO', () => {
+    beforeEach(() => {
+      userDTO = toUserDTO(user);
+    });
+
+    it('should convert UserDTO back to User', () => {
+      const userFromDTO = toUserFromDTO(userDTO, {
+        hashedPassword: user.hashedPassword,
+      });
+
+      expect(userFromDTO).toBeInstanceOf(User);
+
+      expect(userFromDTO.id).toEqual(user.id);
+      expect(userFromDTO.name).toEqual(user.name);
+      expect(userFromDTO.email).toEqual(user.email);
+    });
+
+    it('should include password-related properties when converting back to User', () => {
+      const passwordProps = {
+        passwordChangedAt: new Date(),
+        passwordResetToken: 'resetToken',
+        passwordResetTokenExpiresAt: new Date(),
+      };
+
+      const userWithPasswordProps = createTestUser({
+        passwordChangedAt: passwordProps.passwordChangedAt,
+        passwordResetToken: passwordProps.passwordResetToken,
+        passwordResetTokenExpiresAt: passwordProps.passwordResetTokenExpiresAt,
+      });
+
+      const userFromDTO = toUserFromDTO(userDTO, {
+        hashedPassword: userWithPasswordProps.hashedPassword,
+        ...passwordProps,
+      });
+
+      expect(userFromDTO).toBeInstanceOf(User);
+
+      expect(userFromDTO.hashedPassword).toEqual(userWithPasswordProps.hashedPassword);
+      expect(userFromDTO.passwordChangedAt).toEqual(passwordProps.passwordChangedAt);
+      expect(userFromDTO.passwordResetToken).toEqual(passwordProps.passwordResetToken);
+      expect(userFromDTO.passwordResetTokenExpiresAt).toEqual(
+        passwordProps.passwordResetTokenExpiresAt,
+      );
     });
   });
 });
